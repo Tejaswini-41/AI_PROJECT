f9727350d10b0541b94a2bb684714431318a1d74
Tejaswini Durge <tejaswini.22210270@viit.ac.in>
2024-10-05 11:11:36 +0530

    upto date

--- server/models/Task2.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const task2Schema = new mongoose.Schema({
-    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    assignmentFiles: [String], // Array to store file paths
-});
-
-const Task2 = mongoose.model('Task2', task2Schema);
-export default Task2;
